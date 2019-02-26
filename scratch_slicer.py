7ea5af2ed7acc1344df4bdea07faf8fcc68e45eb
Elle Kiernan <47838809+ellekiernan@users.noreply.github.com>
2019-02-25 20:43:52 -0500

    I've made a mess and I don't know what's going on anymore (with commented out code)

--- scratch_slicer.py
+++ scratch_slicer.py
@@ -64,32 +64,68 @@ def makeSlices(slice):
     next_slice = 0
 
     row1, col1, row2, col2 = slice
-    t, m, total = count(slice)
-        
-    if total < 2 * minIngredients:
-        print("SAD")
-
-
+    cells = (row2 + 1) * (col2 + 1)
     if isValid(slice):
         slice_list.append(slice)
-   
+
     elif isValid(slice) == False:
-        new_slice_top = (row1, col1, (row1+row2)//2, col2)
-        new_slice_bottom = ((row1+row2)//2 + 1, col1, row2, col2)
-        new_slice_left = (row1, col1, row2, (col1+col2)//2)
-        new_slice_right = (row1, (col1+col2)//2 + 1, row2, col2)
+        greatest_valid_boxes = 0
+        g_v_b_coords = []
+        solution_dict = {}
 
-        if isValid(new_slice_top) or isValid(new_slice_bottom):
-            makeSlices(new_slice_top)
-            makeSlices(new_slice_bottom)
+    for i in range(row2 + 1):
+        valid_boxes = 0
+        for j in range(col2 + 1):
+            print(i, j)
+            if i == 0 and j == 0:
+                break
+            else:
+                slice_top_left = (row1, col1, i, j)
+                makeSlices(slice_top_left)
+                if j < col2:
+                    slice_top_right = (row1, j+1, i, col2)
+                    makeSlices(slice_top_right)
+                if i < row2:
+                    slice_bot_left = (i+1, col1, row2, j)
+                    makeSlices(slice_bot_left)
+                if i < row2 and j < col2:
+                    slice_bot_right = (i+1, j+1, row2, col2)
+                    makeSlices(slice_bot_right)
+                if total < 2 * minIngredients:
+                    break
+        
 
-        elif isValid(new_slice_left) or isValid(new_slice_right):
-            makeSlices(new_slice_left)
-            makeSlices(new_slice_right)
+
+
+        #         if isValid(slice_top_left):
+        #             valid_boxes+=1
+        #         if isValid(slice_top_right):
+        #             valid_boxes+=1
+        #         if isValid(slice_bot_left):
+        #             valid_boxes+=1
+        #         if isValid(slice_bot_right):
+        #             valid_boxes+=1
+        #         if valid_boxes > greatest_valid_boxes:
+        #             greatest_valid_boxes = valid_boxes
+        #             g_v_b_coords = [i, j]
+        # print("best split: ", g_v_b_coords)
+
+    # elif isValid(slice) == False:
+    #     new_slice_top = (row1, col1, (row1+row2)//2, col2)
+    #     new_slice_bottom = ((row1+row2)//2 + 1, col1, row2, col2)
+    #     new_slice_left = (row1, col1, row2, (col1+col2)//2)
+    #     new_slice_right = (row1, (col1+col2)//2 + 1, row2, col2)
+
+    #     if isValid(new_slice_top) or isValid(new_slice_bottom):
+    #         makeSlices(new_slice_top)
+    #         makeSlices(new_slice_bottom)
+
+    #     elif isValid(new_slice_left) or isValid(new_slice_right):
+    #         makeSlices(new_slice_left)
+    #         makeSlices(new_slice_right)
             
 
 first_slice = (0, 0, len(pizza) - 1, len(pizza[0]) - 1)
-print("First_slice: ", first_slice)
 makeSlices(first_slice)
 
 # slice1 = (0, 0, 2, 1)
